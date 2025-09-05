1f086b06908ce61522ae4d3e96b28b5a31793247
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-09-05 18:56:47 +0200

    Phonebook backend - step 7

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 app.use(express.json());
-
+app.use(morgan('tiny'));
 const PORT = process.env.PORT || 3001;
 
 let data = [
