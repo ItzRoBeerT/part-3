e98e5ec95463383cd472eb3f3a56935003a763d4
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-09-24 10:45:59 +0200

    Error handling middleware

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -40,33 +40,28 @@ app.get('/info', async (request, response) => {
     ${current_time}`);
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
 	const id = request.params.id;
 	console.log(id);
-	User.findById(id).then((person) => {
-		if (person) {
+	User.findById(id)
+		.then((person) => {
 			response.json(person);
-		} else {
-			response.status(404).json({
-				error: "Id doesn't exist",
-			});
-		}
-	});
+		})
+		.catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
 	const id = request.params.id;
 	User.findByIdAndDelete(id)
 		.then(() => {
 			response.status(204).end();
 		})
 		.catch((error) => {
-			console.error(error);
-			response.status(400).send({ error: 'malformatted id' });
+			next(error);
 		});
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const personData = request.body;
 
 	if (!personData) {
@@ -97,11 +92,21 @@ app.post('/api/persons', (request, response) => {
 				});
 			})
 			.catch((error) => {
-				console.error(error);
-				response.status(500).json({ error: 'Internal server error' });
+				next(error);
 			});
 	}
 });
+
+const errorHandler = (error, request, response, next) => {
+	console.log('nombre del error', error.name);
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
